4d3b67b2535042324026cb257cb7c5b815f18234
waltersequeira <waltersequeira654@gmail.com>
2026-06-14 20:28:20 -0600

    Limpiar imports innecesarios

--- src/components/navegacion/Encabezado.jsx
+++ src/components/navegacion/Encabezado.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
-import logo from "../../assets/comunista.png";
 import { supabase } from "../../database/supabaseconfig";
 import ChatIA from "../ia/ChatIA";
 import Dashboard from "../../views/Dashboard";
